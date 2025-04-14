b94c1b8ee180068b67b0873d34b5baad0a40edc9
Adritrader <adri_denia_123@hotmail.com>
2025-04-14 11:56:29 +0200

    first commit

---
+++ src/Components/Assets/AppWeather.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './AppWeather.css';
+
+export const AppWeather = () => {
+  return (
+    <div className='weather'>
+      <div className='search-bar'>
+        <input type='text' placeholder='Buscar' />
+          <img src='' alt=''>
+        
+        
+          </img>
+        </div>  
+      
+      
+    
+    </div>
+  )
+}
